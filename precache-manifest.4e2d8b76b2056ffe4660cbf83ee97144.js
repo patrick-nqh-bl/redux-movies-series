4f0f4faedee4d729d9fd87a0f91f356299cd6066
Patrick <patrick204nqh@gmail.com>
2020-04-27 16:35:37 +0700

    Updates

---
+++ precache-manifest.4e2d8b76b2056ffe4660cbf83ee97144.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b1ed1e1898f46bb2b3dc21b6294b6f",
+    "url": "/redux-movies-series/index.html"
+  },
+  {
+    "revision": "1a6c121f1845255df2cb",
+    "url": "/redux-movies-series/static/css/main.c8cbbb9f.chunk.css"
+  },
+  {
+    "revision": "3b6ef4142eb59b20baf1",
+    "url": "/redux-movies-series/static/js/2.9fb8474a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-movies-series/static/js/2.9fb8474a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6c121f1845255df2cb",
+    "url": "/redux-movies-series/static/js/main.3763d7c6.chunk.js"
+  },
+  {
+    "revision": "a2398e097f9607fa3349",
+    "url": "/redux-movies-series/static/js/runtime-main.3555ac00.js"
+  },
+  {
+    "revision": "205810b058927b81713c0b8331c62576",
+    "url": "/redux-movies-series/static/media/spinner.205810b0.gif"
+  }
+]);
